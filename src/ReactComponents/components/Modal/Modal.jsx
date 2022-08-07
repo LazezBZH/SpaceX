76b0b86fc9afbc8f9963bd409fb8247e5cabc703
LazezBZH <lazez.bzh@caramail.fr>
2022-08-07 14:55:55 +0200

    reponsive video

--- src/ReactComponents/components/Modal/Modal.jsx
+++ src/ReactComponents/components/Modal/Modal.jsx
@@ -9,11 +9,44 @@ export default function Modal(props) {
       autoplay: 1,
     },
   };
+
+  const optsSmall = {
+    height: "390",
+    width: "573",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const optsXSmall = {
+    height: "290",
+    width: "430",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const optsXXSmall = {
+    height: "200",
+    width: "300",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className="modal">
       <button onClick={props.close}>&#x274C;</button>
       <div className="video-youtube">
         <YouTube videoId={props.videoId} opts={opts} />
+      </div>
+      <div className="video-youtube_small">
+        <YouTube videoId={props.videoId} opts={optsSmall} />
+      </div>
+      <div className="video-youtube_xsmall">
+        <YouTube videoId={props.videoId} opts={optsXSmall} />
+      </div>
+      <div className="video-youtube_xxsmall">
+        <YouTube videoId={props.videoId} opts={optsXXSmall} />
       </div>
     </div>
   );
